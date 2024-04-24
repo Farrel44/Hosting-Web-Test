3007052abaae05b60b4a007358048cb444380332
Farrel44 <farrellasso69@gmail.com>
2024-04-24 20:32:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//About me Animation
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablinks of tablinks){
+        tablinks.classList.remove("active-link");
+    }
+    for(tabcontents of tabcontents){
+        tabcontents.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
